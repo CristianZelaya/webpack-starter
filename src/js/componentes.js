2bd7f6c282e7ad5cacea6cb290f7e833e5026dee
CristianZelaya <criszel94@gmail.com>
2021-04-10 08:46:56 -0600

    webpack server

---
+++ src/js/componentes.js
@@ -0,0 +1,12 @@
+import '../css/componetes.css';
+
+export const saludar = ( nombre ) => {
+
+    console.log('Creando etiqueta h1');
+
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `Hola, ${ nombre }, como estas`;
+
+    document.body.append(h1);
+
+}
